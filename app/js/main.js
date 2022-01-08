6bf82d913a23a111fb542799c8e01a670cb07be3
Melkorich <Kirill.Lavrov92@gmail.com>
2022-01-08 12:20:50 +0300

    header, slider, benefits, tabs, quote sections added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
+
+  $('.hero-slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+  });
+
+  $('.process__tabs-btn').on('click', function(e) {
+    e.preventDefault();
+    $('.process__tabs-btn').removeClass('process__tabs-btn--active');
+    $(this).addClass('process__tabs-btn--active');
+
+    $('.process-item').removeClass('process-item--active');
+    $($(this).attr('href')).addClass('process-item--active');
+  })
  
 });
